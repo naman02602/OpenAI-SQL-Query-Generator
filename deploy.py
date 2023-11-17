2cf448c1f53e210535f7d1a10a5f97fbfa331335
Jagruti Agrawal <jagruti1906@gmail.com>
2023-11-17 02:28:25 -0500

    UDFs complete

--- deploy.py
+++ deploy.py
@@ -9,21 +9,23 @@ if len(sys.argv) != 2:
 
 root_directory = sys.argv[1]
 print(f"Deploying all Snowpark apps in root directory {root_directory}")
+print(os.walk(root_directory))
 
 # Walk the entire directory structure recursively
 for (directory_path, directory_names, file_names) in os.walk(root_directory):
     # Get just the last/final folder name in the directory path
+    print(directory_path, directory_names, file_names)
     base_name = os.path.basename(directory_path)
 
     # Skip any folders we want to ignore
     if base_name in ignore_folders:
-#        print(f"Skipping ignored folder {directory_path}")
+        print(f"Skipping ignored folder {directory_path}")
         continue
 
     # An app.toml file in the folder is our indication that this folder contains
     # a snowcli Snowpark App
     if not "app.toml" in file_names:
-#        print(f"Skipping non-app folder {directory_path}")
+        print(f"Skipping non-app folder {directory_path}")
         continue
 
     # Next determine what type of app it is
--- runsql.sh
+++ runsql.sh
@@ -6,16 +6,3 @@ snowsql -c dev -f modules/teardown.sql
 
 echo Setup Snowflake Account
 snowsql -c dev -f modules/setup_snow.sql
-
-echo Load Raw data
-snowsql -c dev -f modules/load_raw.sql
-
-echo Clean Raw data
-snowsql -c dev -f modules/data_cleaning.sql
-
-echo Run usecase scripts
-snowsql -c dev -f modules/airport_crowd.sql
-snowsql -c dev -f modules/hourly_crowd.sql
-snowsql -c dev -f modules/carbon_footprint.sql
-
-echo End of script
